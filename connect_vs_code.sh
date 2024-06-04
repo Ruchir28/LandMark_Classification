3e9599d619330321a780cca5bc15ebf5d1f49435
Ruchir28 <ruchirnakra28@gmail.com>
2024-06-04 17:44:36 +0530

    Setup Script for remote colab

---
+++ connect_vs_code.sh
@@ -0,0 +1,14 @@
+curl -Lk 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli.tar.gz
+tar -xf vscode_cli.tar.gz
+./code tunnel
+## install miniconda
+mkdir -p ~/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm -rf ~/miniconda3/miniconda.sh
+~/miniconda3/bin/conda init bash
+~/miniconda3/bin/conda init zsh
+## create a conda environment named udacity with python 3.7
+~/miniconda3/bin/conda create --name udacity python=3.7.6 pytorch=1.11.0 torchvision torchaudio cudatoolkit -c pytorch
+conda activate udacity
+pip install -r requirements.txt
